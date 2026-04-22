40913ba59e158411ef6786d78b2add77bcb362ef
FarisEka <fariseka3@gmail.com>
2026-04-22 13:37:59 +0700

    feat: Add Akun Groups and Anak Akuns management functionality
    - Implemented Edit, List, and View pages for Akun Groups.
    - Created schemas for Akun Group forms and infolists.
    - Developed relation manager for Anak Akuns within Akun Groups.
    - Added CRUD functionality for Anak Akuns with appropriate pages and schemas.
    - Introduced Induk Akun management with relation managers for Anak Akuns and Sub Anak Akuns.
    - Established schemas and tables for Induk Akuns and Sub Anak Akuns.
    - Enhanced models with additional methods for better data handling.

---
+++ app/Filament/Resources/IndukAkuns/IndukAkunResource.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Resources\IndukAkuns;
+
+use App\Filament\Resources\AkunGroups\RelationManagers\AnakAkunsRelationManager;
+use App\Filament\Resources\IndukAkuns\Pages\CreateIndukAkun;
+use App\Filament\Resources\IndukAkuns\Pages\EditIndukAkun;
+use App\Filament\Resources\IndukAkuns\Pages\ListIndukAkuns;
+use App\Filament\Resources\IndukAkuns\Pages\ViewIndukAkun;
+use App\Filament\Resources\IndukAkuns\RelationManagers\SubAnakAkunRelationManager;
+use App\Filament\Resources\IndukAkuns\Schemas\IndukAkunForm;
+use App\Filament\Resources\IndukAkuns\Schemas\IndukAkunInfolist;
+use App\Filament\Resources\IndukAkuns\Tables\IndukAkunsTable;
+use App\Models\IndukAkun;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use UnitEnum;
+
+class IndukAkunResource extends Resource
+{
+    protected static ?string $model = IndukAkun::class;
+
+    // protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedClipboardDocumentList;
+
+    protected static string|UnitEnum|null $navigationGroup = 'Master Data';
+
+    protected static ?string $recordTitleAttribute = 'kode_induk_akun';
+
+    public static function form(Schema $schema): Schema
+    {
+        return IndukAkunForm::configure($schema);
+    }
+
+    public static function infolist(Schema $schema): Schema
+    {
+        return IndukAkunInfolist::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return IndukAkunsTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+                //
+            AnakAkunsRelationManager::class,
+            SubAnakAkunRelationManager::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListIndukAkuns::route('/'),
+            'create' => CreateIndukAkun::route('/create'),
+            'view' => ViewIndukAkun::route('/{record}'),
+            'edit' => EditIndukAkun::route('/{record}/edit'),
+        ];
+    }
+}
