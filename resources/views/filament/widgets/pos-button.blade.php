00ffd22da98199de0c6db2b974894b16eaf08ed8
khamal45 <khamal45@proton.me>
2026-05-05 08:18:37 +0700

    fix ukuran button in dashboard

--- resources/views/filament/widgets/pos-button.blade.php
+++ resources/views/filament/widgets/pos-button.blade.php
@@ -1,10 +1,9 @@
 <x-filament-widgets::widget>
-    <x-filament::section class="flex items-center justify-center">
+    <x-filament::section>
         <x-filament::button
             icon="heroicon-o-calculator"
             color="primary"
             size="xl"
-            class="w-full h-32 text-3xl font-bold gap-4"
             x-on:click="window.location.href='{{
                 route('filament.admin.resources.penjualans.pos')
             }}'"
